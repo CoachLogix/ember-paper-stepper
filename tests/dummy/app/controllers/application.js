84c520d3dfe92e376680bc01667b876f71d0d844
Miguel Andrade <jmandrade0@gmail.com>
2017-03-06 20:27:59 +0000

    tweaks to demo page

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,14 +1,17 @@
 import Ember from 'ember';
-const { Controller } = Ember;
+const { Controller, computed } = Ember;
 
 export default Controller.extend({
   linear: true,
   vertical: true,
 
+  clampedStep: computed.reads('currentStep'),
+
   actions: {
     toggle(propName) {
       this.toggleProperty(propName);
     },
+
     validityChange(isValid, isTouched, isInvalidAndTouched) {
       this.set('isInvalid', isInvalidAndTouched);
     }
